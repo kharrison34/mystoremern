235027d79f2223969e0bcfda84a4e6c0f832b43a
Kyle Harrison <kyle.harrison344@gmail.com>
2020-10-03 15:14:11 -0400

    Navbar added with react strap. icons not working for cart or user

---
+++ frontend/src/components/Header.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import {
+  Button,
+  FormControl,
+  Navbar,
+  NavDropdown,
+  Nav,
+  Container,
+} from 'react-bootstrap';
+
+export const Header = () => {
+  return (
+    <header>
+      <Navbar bg='dark' variant='dark' expand='lg' collapseOnSelect>
+        <Container>
+          <Navbar.Brand href='/'>Halloween Haunt</Navbar.Brand>
+          <Navbar.Toggle aria-controls='basic-navbar-nav' />
+          <Navbar.Collapse id='basic-navbar-nav'>
+            <Nav className='ml-auto'>
+              <Nav.Link href='/cart'>
+                <i classsName='fas fa-shopping-cart'></i>Cart
+              </Nav.Link>
+              <Nav.Link href='/login'>
+                <i classsName='fas fa-user'></i>Sign In
+              </Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </header>
+  );
+};
+
+export default Header;
